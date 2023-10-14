4481f97b102253eef416c8408f3bcaa8a3a730ba
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-10-14 08:39:12 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "styled-components/native";
+import { AppProvider, UserProvider } from "@realm/react";
+import { StatusBar } from "react-native";
+import { REALME_APP_ID } from "@env";
+import theme from "./src/theme";
+import { SignIn } from "./src/screens/signIn";
+import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontLoaded) {
+    return;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppProvider id={REALME_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={"transparent"}
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
